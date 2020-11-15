324661e9c529445f12b63b5a417434ec68a786f2
gotomts <mh.goto.web@gmail.com>
2020-11-16 00:48:34 +0900

    feat: add env-cmd sample

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,11 @@
-console.log("Hello TypeScript!");
+const env = process.env;
+
+/**
+ * メイン処理
+ */
+const main = () => {
+  console.log(env.ENV1);
+  console.log("Hello TypeScript!!");
+};
+
+main();
